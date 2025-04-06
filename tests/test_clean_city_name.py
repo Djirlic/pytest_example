8f6ea7ddd915f6c6472a15bcaba7c53f4a7142f8
Manuel Djirlic <freelance@djirlic.com>
2025-04-07 00:30:04 +0200

    CHANGE update test name

--- tests/test_clean_city_name.py
+++ tests/test_clean_city_name.py
@@ -9,5 +9,5 @@ def test_clean_city_name_with_empty_string_returns_unknown():
 def test_clean_city_name_with_city_returns_city_in_title_case():
     assert clean_city_name("united states") == "United States"
   
-def test_clean_city_name_with_whitespace_returns_unknown():
+def test_clean_city_name_with_whitespace_returns_city_in_title_case():
     assert clean_city_name(" london ") == "London"
